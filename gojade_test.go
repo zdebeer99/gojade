3fe2e36639df26651592147172a205fd82acd499
zdebeer <zdebeer@gmail.com>
2015-04-12 19:02:05 +0200

    gojade test

--- gojade_test.go
+++ gojade_test.go
@@ -12,25 +12,25 @@ func TestRenderFiles(t *testing.T) {
 	jade.ViewPath = "res"
 	data := map[string]interface{}{"PageTitle": "Test Jade", "YouAreUsingJade": true, "Children": []string{"Mike", "Sue", "Helen"}}
 	save("res/html/test.html", jade.RenderFile("test", data).Bytes())
-	/*
-		//extends
-		t.Log("Render extends.html")
-		jade.ViewPath = "res/extends"
-		save("res/html/extends.html", jade.RenderFile("index", nil).Bytes())
 
-		//inheritance
-		t.Log("Render inheritance")
-		jade.ViewPath = "res/inheritance"
-		data = map[string]interface{}{"title": "List of Pets", "pets": []string{"Dog", "Cat", "Bird"}}
-		save("res/html/inheritance_a.html", jade.RenderFile("page-a", data).Bytes())
-		save("res/html/inheritance_b.html", jade.RenderFile("page-b", data).Bytes())
+	//extends
+	t.Log("Render extends.html")
+	jade.ViewPath = "res/extends"
+	save("res/html/extends.html", jade.RenderFile("index", nil).Bytes())
 
-		//includes
-		t.Log("Render includes")
-		jade.ViewPath = "res/includes"
-		save("res/html/include.html", jade.RenderFile("index", data).Bytes())
-		save("res/html/include_text.html", jade.RenderFile("index_text", data).Bytes())
-	*/
+	//inheritance
+	t.Log("Render inheritance")
+	jade.ViewPath = "res/inheritance"
+	data = map[string]interface{}{"title": "List of Pets", "pets": []string{"Dog", "Cat", "Bird"}}
+	save("res/html/inheritance_a.html", jade.RenderFile("page-a", data).Bytes())
+	save("res/html/inheritance_b.html", jade.RenderFile("page-b", data).Bytes())
+
+	//includes
+	t.Log("Render includes")
+	jade.ViewPath = "res/includes"
+	save("res/html/include.html", jade.RenderFile("index", data).Bytes())
+	save("res/html/include_text.html", jade.RenderFile("index_text", data).Bytes())
+
 }
 
 func load(filename string) (string, error) {
